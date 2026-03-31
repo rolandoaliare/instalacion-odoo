dce16783f365955ba27147da0ee2e8ad373a6706
ASR <68700448+rolandoaliare@users.noreply.github.com>
2026-03-31 17:26:03 -0300

    Refactor: rename ODOO_USER to odoo_user and fix typos

--- 2-instalar-odoo.sh
+++ 2-instalar-odoo.sh
@@ -21,35 +21,35 @@ echo -----------------------
 wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.focal_amd64.deb
 sudo apt -y install ./wkhtmltox_0.12.5-1.focal_amd64.deb
 
-ODOO_USER=odoo
-echo CREANDO USUARIO $ODOO_USER
+odoo_user=odoo
+echo CREANDO USUARIO $odoo_user
 echo -----------------------
-sudo adduser --system --quiet --shell=/bin/bash --home=/opt/$ODOO_USER --group ${ODOO_USER}
-ls /opt/$ODOO_USER
+sudo adduser --system --quiet --shell=/bin/bash --home=/opt/$odoo_user --group ${odoo_user}
+ls /opt/$odoo_user
 echo
 echo INSTALANDO POSTGRESQL
 echo -----------------------
 sudo apt install -y postgresql postgresql-server-dev-all
-echo CREANDO DATABASE USER $ODOO_USER.
+echo CREANDO DATABASE USER $odoo_user.
 echo -----------------------
 echo 
-sudo su - postgres -c "createuser --no-superuser --createdb --login $ODOO_USER" 2> /dev/null || true
+sudo su - postgres -c "createuser --no-superuser --createdb --login $odoo_user" 2> /dev/null || true
 echo
 echo ODOO
 echo -----------------------
-cd /opt/$ODOO_USER
+cd /opt/$odoo_user
 sudo git clone https://github.com/odoo/odoo.git --depth 1 --branch 15.0 --single-branch 
-sudo chown -R $ODOO_USER:$ODOO_USER /opt/$ODOO_USER
-sudo su - odoo -c "cd /opt/$ODOO_USER/odoo && pip3 install -r requirements.txt"
+sudo chown -R $odoo_user:$odoo_user /opt/$odoo_user
+sudo su - odoo -c "cd /opt/$odoo_user/odoo && pip3 install -r requirements.txt"
 echo
 echo POST-INSTALLATION
 echo -----------------------
-sudo mkdir -p /var/log/$ODOO_USER
-sudo chown $ODOO_USER:root /var/log/$ODOO_USER
-read -p "Nombre DB[$ODOO_USER]:" DB_NAME
-DB_NAME=${DB_NAME:-$ODOO_USER}
+sudo mkdir -p /var/log/$odoo_user
+sudo chown $odoo_user:root /var/log/$odoo_user
+read -p "Nombre DB[$odoo_user]:" DB_NAME
+DB_NAME=${DB_NAME:-$odoo_user}
 echo "La base de datos será $DB_NAME"
-CONFIG_FILE=/etc/$ODOO_USER.conf
+CONFIG_FILE=/etc/$odoo_user.conf
 if [ -f "$CONFIG_FILE" ]; then
 	sudo rm -f $CONFIG_FILE
 fi
@@ -61,8 +61,8 @@ db_host = False
 db_port = False
 db_user = False
 db_password = False
-addons_path = /opt/$ODOO_USER/odoo/addons
-logfile = /var/log/$ODOO_USER/odoo.log
+addons_path = /opt/$odoo_user/odoo/addons
+logfile = /var/log/$odoo_user/odoo.log
 unaccent = True
 db_name = $DB_NAME
 dbfilter = $DB_NAME
@@ -71,12 +71,12 @@ http_interface = 127.0.0.1
 proxy_mode = True
 EOF
 
-sudo chown $ODOO_USER: /etc/$ODOO_USER.conf
-sudo chmod 640 /etc/$ODOO_USER.conf
+sudo chown $odoo_user: /etc/$odoo_user.conf
+sudo chmod 640 /etc/$odoo_user.conf
 
 echo ODOO SERVICE
 echo -----------------------
-SERVICE_FILE=/etc/systemd/system/$ODOO_USER.service
+SERVICE_FILE=/etc/systemd/system/$odoo_user.service
 if [ -f "$SERVICE_FILE" ]; then
 	sudo rm -f $SERVICE_FILE
 fi
@@ -89,26 +89,26 @@ sudo tee -a $SERVICE_FILE <<EOF > /dev/null
 [Service]
   SyslogIdentifier=odoo15
   PermissionStartOnly=true
-  User=$ODOO_USER
-  Group=$ODOO_USER
+  User=$odoo_user
+  Group=$odoo_user
   StandarOuput=journal+console
   Restart=always
   RestartSec=3
   Type=exec
-  User=$ODOO_USER
-  ExecStart=/usr/bin/env python3 /opt/$ODOO_USER/odoo/odoo-bin -c $CONFIG_FILE
+  User=$odoo_user
+  ExecStart=/usr/bin/env python3 /opt/$odoo_user/odoo/odoo-bin -c $CONFIG_FILE
 [Install]
   WantedBy=default.target
 EOF
 
-sudo chmod 0644 /etc/systemd/system/odoo.service
+sudo chmod 0644 $SERVICE_FILE
 
-sudo mkdir -p /var/opt/$odoouser/data_dir
-sudo chown -R $ODOO_USER:$ODOO_USER
+sudo mkdir -p /var/opt/$odoo_user/data_dir
+sudo chown -R $odoo_user:$odoo_user /var/opt/$odoo_user/data_dir
 
-sudo su - odoo -c "/usr/bin/env python3 /opt/$ODOO_USER/odoo/odoo-bin -c $CONFIG_FILE --language es_AR --load-language es_AR --init base,web,point_of_sale --data-dir /var/opt/$ODOO_USER/data_dir --without-demo all --save --db-template template1 --no-database-list --stop-after-init"
+sudo su - odoo -c "/usr/bin/env python3 /opt/$odoo_user/odoo/odoo-bin -c $CONFIG_FILE --language es_AR --load-language es_AR --init base,web,point_of_sale --data-dir /var/opt/$odoo_user/data_dir --without-demo all --save --db-template template1 --no-database-list --stop-after-init"
 
 
 sudo systemctl enable odoo.service # Habilita el servicio para iniciarse con el SO
-echo "Para iniciar el servicio use: 'sudo systemctl start $ODOO_USER.service'"
+echo "Para iniciar el servicio use: 'sudo systemctl start $odoo_user.service'"
 
